3f40fa26078fe35013455f10351ca21e8801c201
SerraZ3 <serra.henrique3@gmail.com>
2021-06-11 20:20:37 -0300

    feat: integrado formulário com backend

--- frontend/src/pages/CreateNivel/index.js
+++ frontend/src/pages/CreateNivel/index.js
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
-
+import axios from "axios";
 function CreateNivel() {
   const [titulo, setTitulo] = useState("");
   const [descricao, setDescricao] = useState("");
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:5000/niveis", {
+        titulo,
+        descricao,
+      });
+      setTitulo("");
+      setDescricao("");
+      alert("Adicionado com sucesso");
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       <h1>Criar nivel</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Titulo"
